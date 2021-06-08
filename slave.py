6b58a6d124414d2cc1a40a23e038521b4a242f9a
Afonso Campos <afonso.campos@ua.pt>
2021-06-08 10:34:20 +0100

    encoding

--- slave.py
+++ slave.py
@@ -1,5 +1,28 @@
 import time
+import base64
 
-while True:
-    print("all your base are belong to us")
-    time.sleep(1)
+#while True:
+print("all your base are belong to us")
+# Exemplo de encode/decode base64 duma mensagem de basic authentication
+# ID = "root"
+# password = "mamacita"
+# authHead = ID+":"+password
+# authHeadb = authHead.encode("ascii")
+# authHeadB64 = base64.b64encode(authHeadb)
+# authHead = f"Authentication: Basic {authHeadB64}"
+# passDecode = base64.b64decode(authHeadB64)
+# passi = passDecode.decode("ascii")
+# print(authHead)
+# print(passDecode)
+# print(passi)
+
+ID = "root"
+password = "mamacita"
+authHead = ID+":"+password
+authHeadb = authHead.encode("ascii")
+authHeadB64 = base64.b64encode(authHeadb)
+authHead = f"Authentication: Basic {authHeadB64}"
+
+
+
+time.sleep(1)
